8b066cfaa658b599976a56efef8800d0ff61b718
tsukanov-as <tsukanov.as@yandex.ru>
2023-02-05 14:38:23 +0300

    fix comment

--- hello_wasm/src/main.go
+++ hello_wasm/src/main.go
@@ -9,7 +9,7 @@ func main() {
 	fmt.Println("Hello there")
 
 	var ns uint64
-	clock_time_get(0, 1000, &ns) // WASI direct call example
+	clock_time_get(0, 1000, &ns) // direct wasi-libc api call example (see: https://github.com/WebAssembly/wasi-libc/blob/8f5275796a82f8ecfd0833a4f3f444fa37ed4546/libc-bottom-half/sources/__wasilibc_real.c#L86)
 
 	fmt.Println("Time from WASI: ", time.Unix(0, int64(ns)))
 }
